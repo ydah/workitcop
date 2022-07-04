06d3d74d4431ad5a6b5bfdbc57f145f39a5df056
ydah <13041216+ydah@users.noreply.github.com>
2022-07-04 09:02:01 +0900

    Add necessary processing to custam cop

--- lib/trialcop.rb
+++ lib/trialcop.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+require_relative "trialcop/inject"
 require_relative "trialcop/version"
 
 module Trialcop
-  class Error < StandardError; end
-  # Your code goes here...
+  PROJECT_ROOT = ::Pathname.new(__dir__).parent.expand_path.freeze
+  CONFIG_DEFAULT = PROJECT_ROOT.join("config", "default.yml").freeze
+  CONFIG = ::YAML.safe_load(CONFIG_DEFAULT.read).freeze
+  private_constant(:CONFIG_DEFAULT, :PROJECT_ROOT)
 end
+
+Trialcop::Inject.defaults!
